f4a47fb5e87cadbcc27d91c551efa6979066da63
adonis butufei <adonis.butufei@gmail.com>
2021-03-23 03:16:30 +0200

    Simplify configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@ module.exports = {
     purge: ["./public/*.html"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {
-        }
+        extend: {}
     },
     variants: {
         extend: {},
