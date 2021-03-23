2a6c204643392c5138500578f8041886997923b7
adonis butufei <adonis.butufei@gmail.com>
2021-03-23 18:48:01 +0200

    extend grid templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
     purge: ["./public/*.html"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {}
+        extend: {
+            gridTemplateRows: {
+                'layout': '96px 1fr'
+            },
+            gridTemplateColumns: {
+                'layout': 'minmax(180px, 1fr) repeat(3, 1fr)'
+            },
+            gridAutoColumns: {
+                'layout': '1fr'
+            }
+        }
     },
     variants: {
         extend: {},
